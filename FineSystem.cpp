393a26c6b4a19edc6a9fabd2aa145745dff08f4f
Musab <67883529+Musab45@users.noreply.github.com>
2023-11-02 20:05:21 +0500

    L

--- FineSystem.cpp
+++ FineSystem.cpp
@@ -3,6 +3,7 @@ using namespace std;
 int main()
 {
 	int days, fine = 1;
+	bool isPaid = false;
 	cout << "Enter Days: ";
 	cin >> days;
 	fine = days * fine;
@@ -13,8 +14,19 @@ int main()
 	else
 	{
 		int over;
+		char answer;
 		over = fine - 7;
 		cout << "Fine is " << over<<"$";
+		cout << "Pay Now?(y/n): ";
+		cin >> answer;
+		if (answer == 'y')
+		{
+			isPaid = true;
+		}
+		else
+		{
+			cout << "Please clear your due fine.";
+		}
 	}
 	return 0;
 }
---
+++ ReturnMechanism.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	bool isPaid = true;
+	if (isPaid)
+	{
+		cout << "Book Returned";
+	}
+	else
+	{
+		cout << "Please pay Fine";
+	}
+}
